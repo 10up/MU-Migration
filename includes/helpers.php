4eacfd5cc6d272f3b4af576d9978c74ecaa37f7e
Nícholas André <nicholas.andre@10up.com>
2016-12-07 22:48:01 -0300

    Somre more refactoring to stop_the_insanity

--- includes/helpers.php
+++ includes/helpers.php
@@ -187,38 +187,57 @@ function light_add_user_to_blog( $blog_id, $user_id, $role ) {
 }
 
 /**
- * The WP_Query class hooks a reference to one of its own methods
- * onto filters if update_post_term_cache or
- * update_post_meta_cache are true, which prevents PHP's garbage
- * collector from cleaning up the WP_Query instance on long-
- * running processes.
- *
- * By manually removing these callbacks (often created by things
- * like get_posts()), we're able to properly unallocate memory
- * once occupied by a WP_Query object.
+ * Free ups memory for long running processes
  */
 function stop_the_insanity() {
-	global $wpdb, $wp_actions, $wp_filter, $wp_version;
+	global $wpdb, $wp_actions, $wp_filter, $wp_object_cache;
 
 	//reset queries
 	$wpdb->queries = array();
 	// Prevent wp_actions from growing out of control
 	$wp_actions = array();
 
-	/*
-	 * WordPress 4.7 has a new Hook infrastructure, so we need to make sure we're accessing the global array properly
-	 */
-	if ( isset( $wp_filter['get_term_metadata'] ) && $wp_filter['get_term_metadata'] instanceof \WP_Hook ) {
-		$filter_callbacks   = &$wp_filter['get_term_metadata']->callbacks;
-	} else {
-		$filter_callbacks   = &$wp_filter['get_term_metadata'];
+	if ( is_object( $wp_object_cache ) ) {
+		$wp_object_cache->group_ops      = array();
+		$wp_object_cache->stats          = array();
+		$wp_object_cache->memcache_debug = array();
+		$wp_object_cache->cache          = array();
+
+		if ( method_exists( $wp_object_cache, '__remoteset' ) ) {
+			$wp_object_cache->__remoteset();
+		}
 	}
 
-	if ( isset( $filter_callbacks[10] ) ) {
-		foreach ( $filter_callbacks[10] as $hook => $content ) {
-			if ( preg_match( '#^[0-9a-f]{32}lazyload_term_meta$#', $hook ) ) {
-				unset( $filter_callbacks[10][ $hook ] );
+	/*
+	 * The WP_Query class hooks a reference to one of its own methods
+	 * onto filters if update_post_term_cache or
+	 * update_post_meta_cache are true, which prevents PHP's garbage
+	 * collector from cleaning up the WP_Query instance on long-
+	 * running processes.
+	 *
+	 * By manually removing these callbacks (often created by things
+	 * like get_posts()), we're able to properly unallocate memory
+	 * once occupied by a WP_Query object.
+	 *
+	 */
+	if ( isset( $wp_filter['get_term_metadata'] ) ) {
+		/*
+		 * WordPress 4.7 has a new Hook infrastructure, so we need to make sure
+	     * we're accessing the global array properly
+		 */
+		if ( $wp_filter['get_term_metadata'] instanceof \WP_Hook ) {
+			$filter_callbacks   = &$wp_filter['get_term_metadata']->callbacks;
+		} else {
+			$filter_callbacks   = &$wp_filter['get_term_metadata'];
+		}
+
+		if ( isset( $filter_callbacks[10] ) ) {
+			foreach ( $filter_callbacks[10] as $hook => $content ) {
+				if ( preg_match( '#^[0-9a-f]{32}lazyload_term_meta$#', $hook ) ) {
+					unset( $filter_callbacks[10][ $hook ] );
+				}
 			}
 		}
 	}
+
 }
